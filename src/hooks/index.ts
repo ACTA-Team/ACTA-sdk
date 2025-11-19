284637c78aebe1b447b66c63911fa2f8db2d374e
JosueBrenes <josuebrenes3005@gmail.com>
2025-11-19 18:57:59 -0300

    feat(hooks): export additional hook modules for vault operations

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,3 +4,7 @@
 export * from "./useCreateCredential";
 export * from "./useCreateVault";
 export * from "./useAuthorizeIssuer";
+export * from "./useConfig";
+export * from "./useVaultApi";
+export * from "./useTxPrepare";
+export * from "./useVaultStore";
